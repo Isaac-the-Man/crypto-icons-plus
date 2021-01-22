ea66ef38e617af45bffd6d3db405d04f63e7404d
Isaac-the-Man <steven97102@gmail.com>
2021-01-22 04:35:15 -0500

    test restructure

--- test/manifest.test.js
+++ test/manifest.test.js
@@ -1,23 +1,25 @@
 const fs = require('fs')
-const dir = './src'
+const dirs = ['./src/16', './src/32', './src/64', './src/128']
 const manifest = 'manifest.json'
 
 test_manifest = test('test manifest vs src', () => {
-	// count src directory
-	let files = fs.readdirSync(dir)
-	let srcNames = []
-	files.forEach((file) => {
-		srcNames.push(file)
-	})
 	// count manifest.json
 	let rawData = fs.readFileSync(manifest)
 	let jsonData = JSON.parse(rawData)
 	let manNames = []
 	jsonData.forEach((coin) => {
-		manNames.push(coin.slug)
+		manNames.push(coin.slug + '.png')
 	})
+	dirs.forEach((dir) => {
+		// count src directory
+		let files = fs.readdirSync(dir)
+		let srcNames = []
+		files.forEach((file) => {
+			srcNames.push(file)
+		})
 
-	expect(srcNames.sort()).toEqual(manNames.sort())
+		expect(srcNames.sort()).toEqual(manNames.sort())
+	})
 })
 
 exports.test_manifest = test_manifest
