578a23acb1c8c63d595b5d6d36e71226489158e1
Isaac-the-Man <steven97102@gmail.com>
2021-04-14 09:09:10 -0400

    Q1 update. Multilple altcoins added.

--- scripts/count.js
+++ scripts/count.js
@@ -1,8 +1,11 @@
 const fs = require('fs')
 const dirs = ['./src/16', './src/32', './src/64', './src/128']
+const man = require('../manifest.json')
 
 dirs.forEach((dir) => {
 	// count src directory
 	let files = fs.readdirSync(dir)
 	console.log(`${dir} contains ${files.length} files`)
 })
+// count manifest
+console.log(`manifest contains ${man.length} files`)
